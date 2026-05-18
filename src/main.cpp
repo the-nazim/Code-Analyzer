4f6ad2562fcbf1ef44547d1e9e2b9e524d995146
the-nazim <thenazim0707@gmail.com>
2026-05-18 18:13:09 +0530

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+
+using namespace clang;
+using namespace clang::tooling;
+
+int main(int args, const char **argv)
+{
+    
+}
