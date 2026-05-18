6219946096d73238bf2eb2d6eae217bc283d7f96
the-nazim <thenazim0707@gmail.com>
2026-05-18 18:34:02 +0530

    phase one working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <llvm/Support/CommandLine.h>
 
 using namespace clang;
 using namespace clang::tooling;
 
-int main(int args, const char **argv)
+static llvm::cl::OptionCategory MyToolCategory("analyzer options");
+
+int main(int argc, const char **argv)
 {
-    
+    auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+
+    if(!ExpectedParser) {
+        llvm::errs() << ExpectedParser.takeError();
+        return 1;
+    }
+
+    CommonOptionsParser& OptionsParser =
+        ExpectedParser.get();
+
+    ClangTool Tool(
+        OptionsParser.getCompilations(),
+        OptionsParser.getSourcePathList()
+    );
+
+    return Tool.run(newFrontendActionFactory<SyntaxOnlyAction>().get());
 }
