b98533361f7a93130a7a1fc7953e2c142baf2ca7
the-nazim <thenazim0707@gmail.com>
2026-05-19 17:37:04 +0530

    fixing bug

--- src/AnalyzerConsumer.cpp
+++ src/AnalyzerConsumer.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "../include/AnalyzerConsumer.h"
 
 void AnalyzerConsumer::HandleTranslationUnit(clang::ASTContext &context) {
+    std::cout << "AST Traversal Started" << std::endl;
     visitor.TraverseDecl(context.getTranslationUnitDecl());
 }
